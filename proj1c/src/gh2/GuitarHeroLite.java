1dc40f758dc3e5e810c0809e570bc9e2b1a7de79
Ishmael17000 <slime_slab@outlook.com>
2025-09-18 15:17:08 +0800

    Add more sound to GuitarHeroLite

--- proj1c/src/gh2/GuitarHeroLite.java
+++ proj1c/src/gh2/GuitarHeroLite.java
@@ -8,21 +8,25 @@ import edu.princeton.cs.algs4.StdDraw;
  */
 public class GuitarHeroLite {
     private static final double CONCERT_A = 440.0;
+    private static final double CONCERT_B = CONCERT_A * Math.pow(2, 2.0 / 12.0);
     private static final double CONCERT_C = CONCERT_A * Math.pow(2, 3.0 / 12.0);
+    private static final double CONCERT_D = CONCERT_A * Math.pow(2, 5.0 / 12.0);
     private static final int WIDTH = 512;
     private static final int HEIGHT = 512;
 
     public static void main(String[] args) {
         /* create two guitar strings, for concert A and C */
         GuitarString stringA = new GuitarString(CONCERT_A);
+        GuitarString stringB = new GuitarString(CONCERT_B);
         GuitarString stringC = new GuitarString(CONCERT_C);
+        GuitarString stringD = new GuitarString(CONCERT_D);
         StdDraw.setCanvasSize(WIDTH, HEIGHT);
         StdDraw.setXscale(0, WIDTH);
         StdDraw.setYscale(0, HEIGHT);
         StdDraw.setPenColor(StdDraw.BLACK);
         StdDraw.setPenRadius(0.05);
         StdDraw.text(WIDTH / 2, (HEIGHT + 16) / 2, "Play the guitar!");
-        StdDraw.text(WIDTH / 2, (HEIGHT - 32) / 2, "Type A or C");
+        StdDraw.text(WIDTH / 2, (HEIGHT - 32) / 2, "Type A to G");
         while (true) {
 
             /* check if the user has typed a key; if so, process it */
@@ -41,18 +45,32 @@ public class GuitarHeroLite {
                     StdDraw.show();
 
                     stringC.pluck();
+                } else if (key == 'b') {
+                    StdDraw.clear();
+                    StdDraw.text(WIDTH / 2, HEIGHT / 2, "B");
+                    StdDraw.show();
+
+                    stringB.pluck();
+                } else if (key == 'd') {
+                    StdDraw.clear();
+                    StdDraw.text(WIDTH / 2, HEIGHT / 2, "D");
+                    StdDraw.show();
+
+                    stringD.pluck();
                 }
             }
 
             /* compute the superposition of samples */
-            double sample = stringA.sample() + stringC.sample();
+            double sample = stringA.sample() + stringB.sample() + stringC.sample() + stringD.sample();
 
             /* play the sample on standard audio */
             StdAudio.play(sample);
 
             /* advance the simulation of each guitar string by one step */
             stringA.tic();
+            stringB.tic();
             stringC.tic();
+            stringD.tic();
 
         }
     }
